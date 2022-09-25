22bc134e44a9d2b09431686c30079aa79db8cad6
tallesweiler <talles.cavalleiro@gmail.com>
2022-09-24 22:59:09 -0300

    att cod

--- Cod Empresa/Empresa/App.java
+++ Cod Empresa/Empresa/App.java
@@ -16,40 +16,39 @@ public class App {
     }
 
     public static void main(String[] args) {
+        Empresa positivo = new Empresa("Positivo");
 
-    Empresa positivo = new Empresa("Positivo");
+        Departamento rh       = new Departamento("Recursos humanos");
+        Departamento vendas   = new Departamento("Vendas");
+        Departamento ti       = new Departamento("Tecnologia da Informação");
+        Departamento diretor  = new Departamento("Diretoria");
 
-    Departamento rh       = new Departamento("Recursos humanos");
-    Departamento vendas   = new Departamento("Vendas");
-    Departamento ti       = new Departamento("Tecnologia da Informação");
-    Departamento diretor  = new Departamento("Diretoria");
+        Funcionarios joão     = new Funcionarios("João Victor", 1212.53f, 1);
+        Funcionarios talles   = new Funcionarios("Talles Cavalleiro", 5000f, 2);
+        Funcionarios celso    = new Funcionarios("Celso Nantes", 400f, 3);
+        Funcionarios bruno    = new Funcionarios("Bruno Santos", 15003f, 4);
+        Funcionarios thiago   = new Funcionarios("Thiago Lahass", 400f, 5);
+        Funcionarios gatti    = new Funcionarios("Gabriel Gatti", 400f, 6);
 
-    Funcionarios joão     = new Funcionarios("João Victor", 1212.53f, 1);
-    Funcionarios talles   = new Funcionarios("Talles Cavalleiro", 5000f, 2);
-    Funcionarios celso    = new Funcionarios("Celso Nantes", 400f, 3);
-    Funcionarios bruno    = new Funcionarios("Bruno Santos", 15003f, 4);
-    Funcionarios thiago   = new Funcionarios("Thiago Lahass", 400f, 5);
-    Funcionarios gatti    = new Funcionarios("Gabriel Gatti", 400f, 6);
+        rh.insereFuncionario(celso);
+        ti.insereFuncionario(joão);
+        ti.insereFuncionario(bruno);
+        vendas.insereFuncionario(thiago);
+        vendas.insereFuncionario(gatti);
+        diretor.insereFuncionario(talles);
 
-    rh.insereFuncionario(celso);
-    ti.insereFuncionario(joão);
-    ti.insereFuncionario(bruno);
-    vendas.insereFuncionario(thiago);
-    vendas.insereFuncionario(gatti);
-    diretor.insereFuncionario(talles);
+        positivo.insereDepartamento(rh);
+        positivo.insereDepartamento(vendas);
+        positivo.insereDepartamento(ti);
+        positivo.insereDepartamento(diretor);
 
-    positivo.insereDepartamento(rh);
-    positivo.insereDepartamento(vendas);
-    positivo.insereDepartamento(ti);
-    positivo.insereDepartamento(diretor);
+        imprimeInformacoesDaEmpresa(positivo);
 
-    imprimeInformacoesDaEmpresa(positivo);
+        imprimeSalarioMedioETotal(positivo);
+        
+        positivo.aumentaSalárioFuncionários(10);
+        diretor.removeFuncionario(talles);
 
-    imprimeSalarioMedioETotal(positivo);
-    
-    positivo.aumentaSalárioFuncionários(10);
-    diretor.removeFuncionario(talles);
-
-    imprimeSalarioMedioETotal(positivo);
+        imprimeSalarioMedioETotal(positivo);
     }
 }
