c607b50fe86f011c395b3d50715ee75e1bef9953
tallesweiler <talles.cavalleiro@gmail.com>
2022-10-01 17:27:25 -0300

    att cod

--- Cod Plano Cartesiano/Plano Cartesiano/App.java
+++ Cod Plano Cartesiano/Plano Cartesiano/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) {
-        
+        PontoCartesiano p1, p2, p3;
+        String aux1, aux2;
+
+        aux1=args[0];
+        aux2=args[1];
+        p1=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
+
+        aux1=args[2];
+        aux2=args[3];
+        p2=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
+
+        aux1=args[4];
+        aux2=args[5];
+        p3=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
+
+        Triangulo t = new Triangulo(p1, p2, p3);
+
+        System.out.println(t.perimetro());
     }
 }
---
+++ Cod Plano Cartesiano/Plano Cartesiano/Triangulo.java
@@ -0,0 +1,40 @@
+public class Triangulo {
+    private PontoCartesiano p1, p2, p3;
+
+    public Triangulo(PontoCartesiano p1, PontoCartesiano p2, PontoCartesiano p3) {
+        this.p1=p1;
+        this.p2=p2;
+        this.p3=p3;
+    }
+
+    public PontoCartesiano getP1() {
+        return p1;
+    }
+    public void setP1(PontoCartesiano p1) {
+        this.p1 = p1;
+    }
+    public PontoCartesiano getP2() {
+        return p2;
+    }
+    public void setP2(PontoCartesiano p2) {
+        this.p2 = p2;
+    }
+    public PontoCartesiano getP3() {
+        return p3;
+    }
+    public void setP3(PontoCartesiano p3) {
+        this.p3 = p3;
+    }
+
+    public double perimetro() {
+        return tamanhoLado(p1, p2)+tamanhoLado(p1, p3)+tamanhoLado(p2, p3);
+    }
+    public double tamanhoLado(PontoCartesiano p1, PontoCartesiano p2) {
+        double x, y;
+        
+        x=p2.getX()-p1.getX();
+        y=p2.getY()-p1.getY();
+        
+        return Math.sqrt(x*x+y*y);
+    }
+}
