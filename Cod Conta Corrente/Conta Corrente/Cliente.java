8d17a4926426ccd79580a304e90253bc165dd1d6
tallesweiler <talles.cavalleiro@gmail.com>
2022-10-01 12:55:20 -0300

    att cod

---
+++ Cod Conta Corrente/Conta Corrente/Cliente.java
@@ -0,0 +1,48 @@
+public class Cliente {
+    private String nome;
+    private double saldo;
+    private int senha;
+
+    public Cliente(String nome, int senha) {
+        this.nome=nome;
+        this.senha=senha;
+    }
+    public Cliente() {}
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public int getSenha() {
+        return senha;
+    }
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public void consultaSaldo() {
+        System.out.printf("Cliente: %s\nSaldo atual: R$%.2d", getNome(), getSaldo());
+    }
+    public void deposito(double valor) {
+        saldo+=valor;
+    }
+
+    @Override
+    public void saque(double valor) {
+        if((valor+valor*0.05)<getSaldo()) {
+            saldo-=(valor+valor*0.05);
+            System.out.println("Saque realizado com sucesso!");
+        }
+        else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+}
---
+++ Cod Conta Corrente/Conta Corrente/ClienteEspecial.java
@@ -0,0 +1,16 @@
+public class ClienteEspecial extends Cliente {
+    public ClienteEspecial(String nome, int senha) {
+        super(nome, senha);
+    }
+
+    @Override
+    public void saque(double valor) {
+        if((valor+valor*0.01)<super.getSaldo()) {
+            super.saldo-=(valor+valor*0.05);
+            System.out.println("Saque realizado com sucesso!");
+        }
+        else {
+            System.out.println("Saldo insuficiente!");
+        }
+    }
+}
