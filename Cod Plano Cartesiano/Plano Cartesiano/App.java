ce07cfca5a93469477ea88466746ce9f0b9f8ad7
tallesweiler <talles.cavalleiro@gmail.com>
2022-10-01 21:28:34 -0300

    att cod

--- Cod Plano Cartesiano/Plano Cartesiano/App.java
+++ Cod Plano Cartesiano/Plano Cartesiano/App.java
@@ -1,19 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        PontoCartesiano p1, p2, p3;
-        String aux1, aux2;
-
-        aux1=args[0];
-        aux2=args[1];
-        p1=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
-
-        aux1=args[2];
-        aux2=args[3];
-        p2=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
-
-        aux1=args[4];
-        aux2=args[5];
-        p3=new PontoCartesiano(Double.parseDouble(aux1), Double.parseDouble(aux2));
+        PontoCartesiano p1=new PontoCartesiano(Double.parseDouble(args[0]), Double.parseDouble(args[1]));
+        PontoCartesiano p2=new PontoCartesiano(Double.parseDouble(args[2]), Double.parseDouble(args[3]));
+        PontoCartesiano p3=new PontoCartesiano(Double.parseDouble(args[4]), Double.parseDouble(args[5]));
 
         Triangulo t = new Triangulo(p1, p2, p3);
 
