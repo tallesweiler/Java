f41e0f929bb0d3de32e94a8d1e2b30393e044e01
Talles Cavalleiro <91703201+tallesweiler@users.noreply.github.com>
2022-10-13 13:38:31 -0300

    att cod

--- Cod Loja/Loja/App.java
+++ Cod Loja/Loja/App.java
@@ -2,17 +2,17 @@ public class App {
     public static void main(String[] args) {
         Loja rihappy = new Loja("Ri Happy");
 
-        Produto banco = new Jogo("Banco Imobiliario", 100, 8);
-        Produto harry = new Livro("Harry Potter e o Prisioneiro de Azkaban", 27.89, "J.K. Rowling");
-        Produto uno = new Jogo("Uno", 15, 5);
-        Produto caixa = new Livro("Caixa de Passaros", 17.95, "Josh Malerman");
-        Produto vida = new Jogo("Jogo da Vida", 99.95, 9);
-
-        rihappy.insereProduto(banco);
+        Produto harry   = new Livro("Harry Potter e o Prisioneiro de Azkaban", 27.89, "J.K. Rowling");
+        Produto caixa   = new Livro("Caixa de Passaros", 17.95, "Josh Malerman");
+        Produto banco   = new Jogo("Banco Imobiliario", 100, 8);
+        Produto vida    = new Jogo("Jogo da Vida", 99.95, 9);
+        Produto uno     = new Jogo("Uno", 15, 5);
+        
         rihappy.insereProduto(harry);
-        rihappy.insereProduto(uno);
         rihappy.insereProduto(caixa);
+        rihappy.insereProduto(banco);
         rihappy.insereProduto(vida);
+        rihappy.insereProduto(uno);
 
         for (Produto p : rihappy.getProdutos()) {
             System.out.println(p);
