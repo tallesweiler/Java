4fa803f35fcd74ed6134f6f8abab6f10c3c1b86c
tallesweiler <talles.cavalleiro@gmail.com>
2022-10-20 22:53:35 -0300

    att cod

--- Cod Loja+/Loja+/src/App.java
+++ Cod Loja+/Loja+/src/App.java
@@ -5,67 +5,100 @@ import java.util.Scanner;
 import livros.Livro;
 
 public class App {
-    public static Livro cadastraLivro(Scanner scan) {
+    public static void menu() {
         System.out.print("\033\143"); //clean
+        System.out.println("==============================");
+        System.out.println("|  1 - Cadastrar livro!      |");
+        System.out.println("|  2 - Listar livros!        |");
+        System.out.println("|  3 - Consultar livro!      |");
+        System.out.println("|  4 - Remover livro!        |");
+        System.out.println("|  0 - Sair!                 |");
+        System.out.println("==============================");
+        System.out.print("Selecione uma opcao: ");
+    }
+
+    public static void cadastraLivro(Scanner scan, Map<String, Livro> livros) {
         Livro l = new Livro();
 
-        System.out.println("Nome do livro: ");
+        System.out.print("Nome do livro: ");
         l.setTitulo(scan.nextLine());
-        System.out.println();
 
-        System.out.println("Nome do autor: ");
+        System.out.print("Nome do autor: ");
         l.setNomeDoAutor(scan.nextLine());
-        System.out.println();
 
-        System.out.println("Ano de publicaçao: ");
+        System.out.print("Ano de publicaçao: ");
         l.setAnoDePublicaçao(scan.nextInt());
         System.out.println();
 
         System.out.println("Livro cadastrado com sucesso!");
 
-        return l;
+        livros.put(l.getTitulo(), l);
+    }
+
+    public static void listaLivros(Map<String, Livro> livros) {
+        for (Livro l : livros.values()) {
+            System.out.println(l);
+        }
+    }
+
+    public static void consultaLivro(Scanner scan, Map<String, Livro> livros) {
+        System.out.println("Livros disponiveis: " + livros.keySet());
+
+        System.out.print("Selecione um livro para consultar: ");
+        String chave=scan.nextLine();
+        
+        System.out.println("\n" + livros.get(chave));
+    }
+
+    public static void removeLivro(Scanner scan, Map<String, Livro> livros) {
+        System.out.println("Livros disponiveis: " + livros.keySet());
+
+        System.out.print("Selecione um livro para remover: ");
+        String chave=scan.nextLine();
+        
+        System.out.println("\n" + livros.get(chave));
+
+        System.out.print("\nTem certeza que deseja remover esse livro? (Digite '1' para remover): ");
+        int resp=scan.nextInt();
+        scan.nextLine();
+        if (resp==1) {
+            livros.remove(chave);
+            System.out.println("O livro foi removido!");
+        }
+        else {
+            System.out.println("O livro nao foi removido!");
+        }
     }
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
-        Map<Integer, Livro> livros = new HashMap<>();
-        int qtdLivros=0;
+        Map<String, Livro> livros = new HashMap<String, Livro>();
 
         while(true) {
-            System.out.print("\033\143"); //clean
-            System.out.println("==============================");
-            System.out.println("|  1 - Cadastrar livro!      |");
-            System.out.println("|  2 - Listar livros!        |");
-            System.out.println("|  3 - Consultar livro!      |");
-            System.out.println("|  4 - Remover livro!        |");
-            System.out.println("==============================");
+            menu();
 
             int x=scan.nextInt();
             scan.nextLine();
+            System.out.print("\033\143");
 
             if (x==1) {
-                Livro livro=cadastraLivro(scan);
-                livros.put(qtdLivros, livro);
-                qtdLivros++;
+                cadastraLivro(scan, livros);
             }
             else if (x==2) {
-                for (Livro l : livros.values()) {
-                    System.out.println(l);
-                }
+                listaLivros(livros);
             }
             else if (x==3) {
-
+                consultaLivro(scan, livros);
             }
             else if (x==4) {
-
+                removeLivro(scan, livros);
             }
             else if (x==0) {
-                System.out.println("Adeus!");
+                System.out.print("\033\143");
                 break;
             }
             else {
                 System.out.println("Opçao inválida!");
-                
             }
 
             System.out.print("\nPressione 'Enter' para sair...");
--- Cod Loja+/Loja+/src/livros/Livro.java
+++ Cod Loja+/Loja+/src/livros/Livro.java
@@ -32,6 +32,6 @@ public class Livro {
     }
 
     public String toString() {
-        return (titulo + " (" + nomeDoAutor + ") " + anoDePublicaçao);
+        return (titulo + " (" + nomeDoAutor + " - " + anoDePublicaçao + ")");
     }
 }
